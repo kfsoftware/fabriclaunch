45f7e929369133409077a245e3544c9689169c41
David VIEJO <dviejo@kungfusoftware.es>
2024-07-23 23:05:17 +0200

    Update docusaurus.config.ts
    Signed-off-by: David VIEJO <dviejo@kungfusoftware.es>

--- apps/fabriclaunch-docs/docusaurus.config.ts
+++ apps/fabriclaunch-docs/docusaurus.config.ts
@@ -9,10 +9,10 @@ const config: Config = {
   themes: ['@docusaurus/theme-mermaid'],
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://kfsoftware.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/fabriclaunch/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
